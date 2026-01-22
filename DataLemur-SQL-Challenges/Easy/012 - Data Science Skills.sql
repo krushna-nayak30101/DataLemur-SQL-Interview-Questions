663f864d4df3fd3ab10528016ead06658717224d
Krushna Nayak <krushna.nayak30101@gmail.com>
2026-01-22 17:58:53 +0530

    Update 012 - Data Science Skills.sql

--- DataLemur-SQL-Challenges/Easy/012 - Data Science Skills.sql
+++ DataLemur-SQL-Challenges/Easy/012 - Data Science Skills.sql
@@ -4,3 +4,20 @@ WHERE skill IN('Python', 'Tableau', 'PostgreSQL')
 GROUP BY candidate_id
 HAVING COUNT(candidate_id) = 3
 ORDER BY candidate_id;
+
+
+-- methid 2 
+
+WITH cte AS (
+  SELECT
+    candidate_id
+    ,STRING_AGG(LOWER(skill), ' , ') AS skills
+  FROM candidates
+  GROUP BY candidate_id
+)
+SELECT
+  candidate_id
+FROM cte 
+WHERE skills LIKE '%python%'
+AND skills LIKE '%tableau%'
+AND skills LIKE '%postgresql%'
