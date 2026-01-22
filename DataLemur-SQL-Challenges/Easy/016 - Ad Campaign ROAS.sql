3ec46a1562fb12f4a4ef2163d3676013dc89682f
Krushna Nayak <krushna.nayak30101@gmail.com>
2026-01-22 18:01:17 +0530

    Update 016 - Ad Campaign ROAS.sql

--- DataLemur-SQL-Challenges/Easy/016 - Ad Campaign ROAS.sql
+++ DataLemur-SQL-Challenges/Easy/016 - Ad Campaign ROAS.sql
@@ -2,3 +2,12 @@
 FROM ad_campaigns
 GROUP BY advertiser_id
 ORDER BY advertiser_id;
+
+
+SELECT
+  advertiser_id,
+  ROUND(((SUM(revenue) / SUM(spend))), 2) AS ROAS
+FROM ad_campaigns_google
+GROUP BY advertiser_id
+ORDER BY advertiser_id;
+
