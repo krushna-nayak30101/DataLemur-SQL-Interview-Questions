8492bf108e3c06612fce32d1562b771ab00e2d3b
Krushna Nayak <krushna.nayak30101@gmail.com>
2026-01-23 15:05:00 +0530

    Update 027 - QuickBooks vs TurboTax!.sql

--- DataLemur-SQL-Challenges/Easy/027 - QuickBooks vs TurboTax!.sql
+++ DataLemur-SQL-Challenges/Easy/027 - QuickBooks vs TurboTax!.sql
@@ -1,4 +1,4 @@
 SELECT
-  SUM (CASE WHEN LOWER(product) LIKE 'turbotax%' THEN 1 ELSE 0 END) AS turbotax_total,
-  SUM (CASE WHEN LOWER(product) LIKE 'quickbooks%' THEN 1 ELSE 0 END) AS quickbooks_total
+  SUM (CASE WHEN LOWER(product) LIKE 'turbotax%' THEN 1 ELSE 0 END) AS turbotax,
+  SUM (CASE WHEN LOWER(product) LIKE 'quickbooks%' THEN 1 ELSE 0 END) AS quickbooks
 FROM filed_taxes;
