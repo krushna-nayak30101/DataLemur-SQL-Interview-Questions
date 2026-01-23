5be6704b34d7c1d04bb402dca9282b7a1c850930
Krushna Nayak <krushna.nayak30101@gmail.com>
2026-01-23 14:16:07 +0530

    Create 031 - Subject Matter Experts .sql

---
+++ DataLemur-SQL-Challenges/Easy/031 - Subject Matter Experts .sql
@@ -0,0 +1,6 @@
+SELECT employee_id
+FROM employee_expertise_accenture
+GROUP BY employee_id
+HAVING (SUM(years_of_experience) >= 8 AND COUNT(DISTINCT domain) = 1) 
+  OR (SUM(years_of_experience) >=12 AND COUNT(DISTINCT domain) = 2);
+
